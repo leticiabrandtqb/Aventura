92da1535e70d9ea775014c86c2fa60c7498ae360
leticiabrandtqb <brandt.leticia@escola.pr.gov.br>
2025-09-04 10:58:14 -0300

    ok

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const avanca = document.querySelectorAll ('.btn-proximo');
 const reiniciarBtn = document.getEelementoById('bnt-reiniciar');
 
-avanca.forEach(button=> 
-button.addEventListener('click')
+avanca.forEach(button=> {
+button.addEventListener('click', function(){
+const atual = document.querySelector('.ativo');
+const proximoPasso ='passo-'+this.getAtrribute('data-proximo');
 
+atual.classList.remove('ativo');
+const proximoElemento= document.getElementById(proximoPasso);
 
-)
+if(proximoElemento){
+    proximoElemento.classList.add('ativo');
+} else{
+    console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
+
+}
+ });
+});
